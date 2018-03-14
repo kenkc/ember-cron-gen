bfa364fb8b508d50378961eea5db9047c746f3b8
kenkc <ken.casey@me.com>
2018-03-14 09:46:07 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cron-gen'
+  name: 'ember-cron-gen',
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('app/styles/ember-cron-gen.css');
+  }
 };
